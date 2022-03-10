dc66319c9b2d09beb686d2a8cec6ee833a8e3719
PROCW <developerteam888@outlook.com>
2022-03-10 15:59:42 -0500

    update the chat login and signup page

--- SwiftUI/ContentView.swift
+++ SwiftUI/ContentView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+
     var body: some View {
-        VStack(){
-            Image("logo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+        NavigationView {
+            ScrollView {
+                
+                VStack{
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "person.crop.circle.fill.badge.plus")
+                            .font(.system(size: 64))
+                            .padding()
+                    }
+                    
+                    
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                    SecureField("Password", text: $password)
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text("Create Account")
+                                .font(.system(size: 21))
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                           Spacer()
+                        }.background(.blue)
+                            
+                    }
+                }.padding()
+                
+                
+                
+                
+                
+                
+                Text("Here is my creation account page")
+            }.navigationTitle("Create Account")
+    
         }
     }
 }
