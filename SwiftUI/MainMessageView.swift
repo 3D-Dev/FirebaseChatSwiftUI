1a38f6e5ae7b23d9e62080e08bf2790173a78f2a
PROCW <developerteam888@outlook.com>
2022-03-11 17:13:21 -0500

    messageView init

---
+++ SwiftUI/MainMessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MainMessageView.swift
+//  FirebaseChat
+//
+//  Created by pro on 2022-03-11.
+//
+
+import SwiftUI
+
+struct MainMessageView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                
+            }
+        }
+    }
+}
